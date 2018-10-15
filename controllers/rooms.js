bbddc3ada3429117ad5a7c4205fb39036ef25359
Charlotte Prevost <chaprevost91@gmail.com>
2018-10-15 16:42:44 -0500

    can make new rooms

--- controllers/rooms.js
+++ controllers/rooms.js
@@ -4,13 +4,46 @@ const router = express.Router();
 
 const Room = require('../models/rooms/rooms');
 
+
+// ******************** INDEX ROUTE ******************** //
+
 router.get('/', (req, res) => {
-	// console.log(req);
-	res.render('../views/rooms/index.ejs')
+	console.log(req);
+	Room.find({},
+		(err, allRooms) => {
+			if(err){console.log(`---------- ERROR ---------- \n`, err);}
+		 	else {
+		 		console.log(`---------- ALL ROOM ---------- \n`, allRooms);
+				res.render('../views/rooms/index.ejs', {
+					rooms: allRooms
+				});
+		 	}
+		})
 });
+
+
+// ******************** NEW ROUTE ******************** //
 
 router.get('/new', (req, res) => {
 	res.render('../views/rooms/new.ejs')
 });
+
+
+// ******************** CREATE ROUTE ******************** //
+
+router.post('/', (req, res) => {
+	Room.create(req.body,
+		(err, createdRoom) => {
+		 	if(err){console.log(`---------- ERROR ---------- \n`, err);}
+		 	else {
+		 		console.log(`---------- CREATED ROOM ---------- \n`, createdRoom);
+		 		res.redirect('/rooms');
+		 	}
+		}
+	)
+})
+
+
+
 
 module.exports = router;
